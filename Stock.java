02916e1d83534af0b84e44f15fd2aa327d320738
amanmeka <110500671+amanmeka@users.noreply.github.com>
2024-05-09 17:25:52 -0500

    Update Stock.java an I think I am done

--- Stock.java
+++ Stock.java
@@ -1,14 +1,37 @@
 import java.util.*;
 public class Stock {
   private double volatility;
-  private double secondForecast;
+  private double firstOrderForecast;
+  private double secondOrderForecast;
   private double stockPrice;
 
   public Stock() {
-
     volatility = (Math.random() * 1) + 10;
-    secondForecast = 
-    
-  
-  
+    secondOrderForecast = (Math.random() * 100) + 1;
+    stockPrice = (Math.random() * 100) + 1;
+    firstOrderForecast = (Math.random()*20)-10;
+  }
+
+  public double newDay() {
+    double change = Math.random() - 0.5;
+    firstOrderForecast += change;
+    return stockPrice - (stockPrice - secondOrderForecast) * (volatility * firstOrderForecast);
+  }
+
+  public double getVolatility() {
+    return volatility;
+  }
+
+  public double getFirstOrderForecast() {
+    return firstOrderForecast;
+  }
+
+  public double getSecondOrderForecast() {
+    return secondOrderForecast;
+  }
+
+  public double getStockPrice() {
+    return stockPrice;
+  }
 }
+  
