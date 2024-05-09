43ffacd11cf06a58aca685087f8e7a4ec69e950e
amanmeka <110500671+amanmeka@users.noreply.github.com>
2024-05-09 15:55:21 -0500

    Update Stock.java

--- Stock.java
+++ Stock.java
@@ -1,6 +1,14 @@
-
+import java.util.*;
 public class Stock {
   private double volatility;
   private double secondForecast;
-  private double stockPrice
+  private double stockPrice;
+
+  public Stock() {
+
+    volatility = (Math.random() * 1) + 10;
+    secondForecast = 
+    
+  
+  
 }
