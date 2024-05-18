01bc31096dae047efeb278db9a51cd3769bbc130
Aman Meka <aman.mekas@gmail.com>
2024-05-17 22:46:19 -0500

    busting rn

---
+++ StockTemplate.java
@@ -0,0 +1,35 @@
+public class StockTemplate {
+    /*
+    //Initialize instance variables
+
+
+
+
+    //Initialization constructor
+
+
+
+
+    //Newday method, simulates one new day of the StockMarket (what methods would you have to call from Stock?)
+    public void newDay() {
+
+    }
+
+
+     
+public double getFunction() {
+}
+
+     //Add ‘add” to sharesInCompany
+     public void addShares(int add) {
+
+     }
+     //Delete 'delete' from sharesInCompany
+     public void sellShares(int delete) {
+
+     }
+
+     /
+      
+public String toString() {//return stockName + “  Stock price: “ + stockPrice + “  Shares in stock: “ + sharesInCompany}    */
+}
